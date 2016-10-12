f6acd8a52f788bb2f090ae8bb842a88d6974579f
Shane Nayler <snayler0@hotmail.com>
2016-10-12 15:00:10 +1100

    better handling of standout CPU (no longer checking memory)

--- lambdas/sandman.py
+++ lambdas/sandman.py
@@ -4,6 +4,45 @@ from __future__ import print_function
 import boto3
 import datetime
 
+def get_metric(instance, metric):
+    """
+    get metric for current instance
+    """
+
+    cw_client = boto3.client('cloudwatch')
+    mins = 20
+    end = datetime.datetime.now()
+    start = end - datetime.timedelta(minutes=mins)
+
+    datapoints = cw_client.get_metric_statistics(Namespace=metric['namespace'],
+                                           MetricName=metric['metricname'],
+                                           Dimensions=[
+                                               {
+                                                   'Name': 'InstanceId',
+                                                   'Value': instance
+                                               },
+                                           ],
+                                           StartTime=start,
+                                           EndTime=end,
+                                           Period=300,
+                                           Statistics=['Average'],
+                                           Unit='Percent'
+                                           )['Datapoints']
+
+    # Get the average from each datapoint that was returned and make an average of those, or set the average to 0.0 if no datapoints were found
+    if datapoints:
+        total = 0.0
+
+        for data in datapoints:
+            total += float(data['Average'])
+
+        average = (total/len(datapoints))
+    else:
+        average = 0.0
+
+    return average
+
+
 def lambda_handler(event, context):
     """
     This lambda script is designed to find and terminate the first autoscaled instance
@@ -14,84 +53,49 @@ def lambda_handler(event, context):
     # Setup variables to use throughout the script
     asg_client = boto3.client('autoscaling')
     ec2_client = boto3.client('ec2')
-    cw_client = boto3.client('cloudwatch')
-    mins = 20
-    threshold = 90.0
     terminated = False
-    end = datetime.datetime.now()
-    start = end - datetime.timedelta(minutes=mins)
-    instance_ids = []
-    metrics = [{'namespace': 'AWS/EC2', 'metricname': 'CPUUtilization'},
-               {'namespace': 'System/Linux', 'metricname': 'MemoryUtilization'}]
+    terminated_instance = None
+    autoscaling_groups = {}
+    metric = {'namespace': 'AWS/EC2', 'metricname': 'CPUUtilization', 'threshold': 90}
 
     # Get all autoscaling instances
     autoscaling_instances = asg_client.describe_auto_scaling_instances()['AutoScalingInstances']
 
-    # Weed out any instances that aren't currently In Service
     for instance in autoscaling_instances:
-        if instance['LifecycleState'] == 'InService':
-            instance_ids.append(instance['InstanceId'])
+        autoscaling_groups.setdefault(instance['AutoScalingGroupName'], []).append(instance['InstanceId'])
 
-    for instance in instance_ids:
+    for asg in autoscaling_groups:
+        print('Checking ASG: {0}'.format(asg))
+        for instance in autoscaling_groups[asg]:
+            print('-----Checking instance: {0}'.format(instance))
+            other_metrics = []
 
-        averages = []
+            curr_metric = get_metric(instance, metric)
+            print('-----Average CPU for this instance: {0}'.format(curr_metric))
 
-        for metric in metrics:
-            curr_metric = cw_client.get_metric_statistics(Namespace=metric['namespace'],
-                                                          MetricName=metric['metricname'],
-                                                          Dimensions=[
-                                                              {
-                                                                  'Name': 'InstanceId',
-                                                                  'Value': instance
-                                                              },
-                                                          ],
-                                                          StartTime=start,
-                                                          EndTime=end,
-                                                          Period=300,
-                                                          Statistics=['Average'],
-                                                          Unit='Percent'
-                                                          )
+            for other_instance in autoscaling_groups[asg]:
+                if other_instance != instance:
+                    other_metrics.append(get_metric(other_instance, metric))
 
-            datapoints = curr_metric['Datapoints']
+            average = (sum(other_metrics)/len(other_metrics)) if len(other_metrics) > 0 else 0.0
 
-            # Get the average from each datapoint that was returned and make an average of those, or set the average to 0.0 if no datapoints were found
-            if datapoints:
-                total = 0.0
-                print('{0} datapoints were returned for metric: {1} on instance: {2}'.format(len(datapoints), metric['metricname'], instance))
+            print('-----Average CPU for other instances sharing same autosacaling group: {0}'.format(average))
 
-                for data in datapoints:
-                    total += float(data['Average'])
+            diff = curr_metric - average
+            print('-----diff = {0}'.format(diff))
 
-                average = (total/len(datapoints))
-            else:
-                average = 0.0
-
-            averages.append(average)
-
-            # Set flags to determine whether to terminate this instance or not
-            cpu_terminate = False
-            mem_terminate = False
-            both_collected = False
-
-            if len(averages) > 1:
-                print('got {0} averages for {1}'.format(len(averages), instance))
-                both_collected = True
-
-            if both_collected and averages[0] > threshold:
-                print('{0} {1} is at {2}'.format(instance, metrics[0]['metricname'], averages[0]))
-                cpu_terminate = True
-
-            if both_collected and averages[1] > threshold:
-                print('{0} {1} is at {2}'.format(instance, metrics[1]['metricname'], averages[1]))
-                mem_terminate = True
-
-            # If an instance has been found that is running high cpu and ram usage and we haven't already terminated an instance...
-            if cpu_terminate and mem_terminate and not terminated:
-                print('Terminating {0}...'.format(instance))
-                ec2_client.terminate_instances(InstanceIds=[instance])
-                terminated = True
+            if curr_metric > metric['threshold'] and diff >= 50 and average != 0.0:
+                print('-----{0} average of {1} is above {2}, and at least 50% higher than the average of the rest of '
+                      'it\'s autoscaling group {3}, which has an average (excluding this instance) of {4}'
+                      .format(instance, curr_metric, metric['threshold'], asg, average))
+                if not terminated:
+                    print('-----Terminating {0}...'.format(instance))
+                    ec2_client.terminate_instances(InstanceIds=[instance])
+                    terminated_instance = instance
+                    terminated = True
 
     if not terminated:
-        print('No autoscaled instances found with cpu or ram outside the threshold of {0}'.format(threshold))
+        print('No autoscaled instances found with out of the ordinary cpu levels')
     else:
-        print('One instance was terminated for having cpu and ram that was measured above the threshold of {0}'.format(threshold))
+        print('One instance ({0}) was terminated for having cpu that had significantly spiked above the rest of '
+              'its autoscaling group'.format(terminated_instance))
